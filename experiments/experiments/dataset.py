b52d5012e4fb86e079ccab9bec0d3b0b5ba351e9
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2024-07-19 04:02:34 -0300

    wip: notebooks

--- experiments/experiments/dataset.py
+++ experiments/experiments/dataset.py
@@ -9,7 +9,10 @@ from zipfile import ZipFile
 
 import pandas as pd
 
-with open(join('.secrets', 'kaggle.json'), 'r', encoding='utf-8') as file:
+KAGGLE_CREDENTIALS_PATH = join('..' ,'.secrets', 'kaggle.json')
+DATA_PATH = join('..', '.data')
+
+with open(KAGGLE_CREDENTIALS_PATH, 'r', encoding='utf-8') as file:
     json = load(file)
     environ['KAGGLE_USERNAME'] = json['username']
     environ['KAGGLE_KEY'] = json['key']
@@ -19,11 +22,11 @@ import kaggle
 kaggle.api.authenticate()
 kaggle.api.competition_download_file('siim-isic-melanoma-classification',
                                      'train.csv',
-                                     path='.data',
+                                     path=DATA_PATH,
                                      quiet=False,
                                      force=False)
 
-with ZipFile(join('.data', 'train.csv.zip'), 'r') as file:
-    file.extractall(join('.data', 'extracted'))
+with ZipFile(join(DATA_PATH, 'train.csv.zip'), 'r') as file:
+    file.extractall(join(DATA_PATH, 'extracted'))
 
-train_data = pd.read_csv(join('.data', 'extracted', 'train.csv'))
+train_data = pd.read_csv(join(DATA_PATH, 'extracted', 'train.csv'))
