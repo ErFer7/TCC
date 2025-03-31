c1743ab645e5740e732252389d16cab00fac6465
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2025-03-31 02:29:41 -0300

    [46]: Splits

--- experiments/notebooks/scripts/data.py
+++ experiments/notebooks/scripts/data.py
@@ -2,13 +2,11 @@
 Preparação dos dados de treinamento.
 '''
 
-from re import search
+from os.path import join
 from json import dump, load
 from tqdm.notebook import tqdm
 from datasets import Dataset
 from sklearn.model_selection import train_test_split
-
-import pandas as pd
 
 PROMPT = 'Classify the skin lesion in the image.'
 ANSWER = 'The skin lesion in the image is {disease}.'
@@ -106,3 +104,82 @@ def generate_test_samples(dataset: Dataset, size: int | None = None) -> list:
             test_samples.append((sample['image'], sample['dx']))
 
     return test_samples
+
+
+def analyse_dataset(data: list, dataset_path: str, dataset_name: str, save: bool = True) -> dict:
+    '''
+    Analisa os dados.
+    '''
+
+    data_analysis = {
+        'total_size': 0,
+        'average_images_per_exam': 0,
+        'elementary_lesions_distribution': {'classes_count': 0, 'classes': {}},
+        'secondary_lesions_distribution': {'classes_count': 0, 'classes': {}},
+        'coloration_distribution': {'classes_count': 0, 'classes': {}},
+        'morphology_distribution': {'classes_count': 0, 'classes': {}},
+        'size_distribution': {'classes_count': 0, 'classes': {}},
+        'local_distribution': {'classes_count': 0, 'classes': {}},
+        'distribution_distribution': {'classes_count': 0, 'classes': {}},
+        'risk_distribution': {'classes_count': 0, 'classes': {}},
+        'skin_lesion_distribution': {'classes_count': 0, 'classes': {}}
+    }
+
+
+    data_analysis['total_size'] = len(data)
+    data_analysis['average_images_per_exam'] = sum(len(exam['images'])
+                                                   for exam in data) / data_analysis['total_size']
+
+    lists = [
+        'elementary_lesions',
+        'secondary_lesions',
+        'coloration',
+        'morphology',
+        'distribution'
+    ]
+
+    strings = [
+        'size',
+        'local',
+        'risk',
+        'skin_lesion'
+    ]
+
+    for exam in data:
+        report = exam['report']
+
+        for data_list in lists:
+            for value in report[data_list]:
+                distribution = data_analysis[f'{data_list}_distribution']
+
+                if value not in distribution['classes']:
+                    distribution['classes_count'] += 1
+                    distribution['classes'][value] = 0
+
+                distribution['classes'][value] += 1
+
+        for data_string in strings:
+            distribution = data_analysis[f'{data_string}_distribution']
+
+            if report[data_string] not in distribution['classes']:
+                distribution['classes_count'] += 1
+                distribution['classes'][report[data_string]] = 0
+
+            distribution['classes'][report[data_string]] += 1
+
+    for key, distribution in data_analysis.items():
+        if key.endswith('_distribution'):
+            classes = distribution['classes']
+
+            for value in classes:
+                classes[value] = {'value': classes[value],
+                                  'proportion': round(classes[value] / data_analysis['total_size'] * 100, 3)}
+
+            sorted_items = sorted(distribution['classes'].items(), key=lambda x: x[1]['value'], reverse=True)
+            data_analysis[key]['classes'] = dict(sorted_items)
+
+    if save:
+        with open(join(dataset_path, f'{dataset_name[:-5]}_analysis.json'), 'w', encoding='utf-8') as file:
+            dump(data_analysis, file, indent=4, ensure_ascii=False)
+
+    return data_analysis
